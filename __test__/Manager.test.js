4bab202f6c108515031ed31fa6c296469512c62c
Chi Chiu Lam <108379616+chichiulam2022@users.noreply.github.com>
2022-09-08 13:38:08 -0400

    Update Manager.test.js

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,24 +1,25 @@
 const Manager = require("../lib/Manager");
 
 test('if manager object will be created', () => {
-    const manager = new Manager('Chi Chiu Lam', '01', 'cclam@gmail.com');
+    const manager = new Manager('Chi Chiu Lam', '01', 'cclam@gmail.com', '101');
     expect(manager.name).toBe('Chi Chiu Lam');
     expect(manager.id).toBe('01');
     expect(manager.email).toBe('cclam@gmail.com');
+    expect(manager.officeNumber).toBe('101');
 });
 
 test("if the manager's name will be created", () => {
-    const manager = new Manager('Chi Chiu Lam', '01', 'cclam@gmail.com');
+    const manager = new Manager('Chi Chiu Lam', '01', 'cclam@gmail.com', '101');
     expect(manager.getName()).toBe('Chi Chiu Lam');
 });
 
 test("if manager's id will be created", () => {
-    const manager = new Manager('Chi Chiu Lam', '01', 'cclam@gmail.com');
+    const manager = new Manager('Chi Chiu Lam', '01', 'cclam@gmail.com', '101');
     expect(manager.getId()).toBe('01');
 });
 
 test("if manager's email will be created", () => {
-    const manager = new Manager('Chi Chiu Lam', '01', 'cclam@gmail.com');
+    const manager = new Manager('Chi Chiu Lam', '01', 'cclam@gmail.com', '101');
     expect(manager.getEmail()).toBe('cclam@gmail.com');
 });
 
